74dbc6a4e9e286bac2b7fe3bad2ffa626eb6128e
Alejandra Lezcano <ma.alexa2000@fpuna.edu.py>
2026-03-18 11:40:46 -0300

    initialize project structure with package.json, package-lock.json, and .gitignore; add TypeScript definitions and interfaces for table components

---
+++ src/table.ts
@@ -0,0 +1,31 @@
+import { PipeTransform } from "@angular/core";
+
+export type ColumnStyleType =
+  | "normal"
+  | "label"
+  | "codeWithDescription"
+  | "actionBadges";
+
+export type ColumnColor =
+  | "primary"
+  | "accent"
+  | "warn"
+  | "success"
+  | "warning"
+  | "";
+
+export interface ColumnItem {
+  title: string;
+  keys: string[];
+  styleType: ColumnStyleType;
+  bold?: boolean;
+  color?: ColumnColor;
+  pipe?: PipeTransform;
+}
+
+export interface CellContentWithColor {
+  text: string;
+  color: ColumnColor;
+}
+
+export type TableCellContent = string | { text: string; color?: ColumnColor };
