e3f8ad287a0c0b56a3ad3fb4ec709bdc074986c6
alezcano <alezcano@excelsis.com.py>
2026-04-07 19:21:54 -0300

    agregamos emptyDetailsResponse

--- src/pagination.interface.ts
+++ src/pagination.interface.ts
@@ -1,25 +1,42 @@
 export interface Pageable {
-    pageNumber: number;
-    pageSize: number;
-    sort: Sort;
-    offset: number;
-    paged: boolean;
-    unpaged: boolean;
-  }
-  
-  export interface Sort {
-    empty: boolean;
-    sorted: boolean;
-    unsorted: boolean;
-  }
-  
-  export interface PaginatedResponse<T> {
-    content: T[];
-    pageable: Pageable;
-    totalPages: number;
-    totalElements: number;
-    last: boolean;
-    size: number;
-    number: number;
-  }
-  
+  pageNumber: number;
+  pageSize: number;
+  sort: Sort;
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+
+export interface Sort {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
+
+export interface PaginatedResponse<T> {
+  content: T[];
+  pageable: Pageable;
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  size: number;
+  number: number;
+}
+
+
+export const emptyDetailsResponse = {
+  content: [],
+  pageable: {
+    pageNumber: 0,
+    pageSize: 10,
+    sort: { empty: true, sorted: false, unsorted: true },
+    offset: 0,
+    paged: true,
+    unpaged: false,
+  },
+  totalPages: 0,
+  totalElements: 0,
+  last: true,
+  size: 10,
+  number: 0,
+};
